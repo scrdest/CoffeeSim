d960fd044ad2c5688a018c0676ce9492a5df51d2
scrdest <jan.k.malek@gmail.com>
2018-09-09 14:40:08 +0200

    More water tank unittests.

--- tests/test_components.py
+++ tests/test_components.py
@@ -4,12 +4,14 @@ import unittest
 
 from CoffeeSim.components import water_supply, bean_supply, grinders, heaters, interfaces
 
+from CoffeeSim import comestibles
+
 def handle_IO(*args, **kwargs): print(*args, **kwargs)
 
 
 class WaterSupplyTest(unittest.TestCase):
 
-    def test_watertank(self):
+    def test_empty_watertank(self):
         empty_tank = water_supply.Tank(contents=None)
         
         # Empty tank acts empty:
@@ -17,6 +19,19 @@ class WaterSupplyTest(unittest.TestCase):
         self.assertEquals(empty_tank.contents_volume, 0)
         with self.assertRaises(RuntimeWarning): self.assertEquals(empty_tank.remove(10), 10)
         print("\nEmpty tank behavior as expected...")
+
+    def test_filled_watertank(self):
+        contents_to_use = {comestibles.Water: water_supply.Tank.capacity}
+        tank = water_supply.Tank(contents=contents_to_use)
+        
+        # Volume detected:
+        self.assertTrue(tank.contents)
+        self.assertGreater(tank.contents_volume, 0)
+        for cont_type, volume in contents_to_use.items():
+            # nasty bit of O(n^2)... sadly necessary, because item contents are instances while we pass in classes as args
+            if volume > 0: self.assertTrue(any((isinstance(cont, cont_type) for cont in tank.contents)))
+        with self.assertRaises(RuntimeWarning): tank.fill(contents_to_use)
+        print("\nFilled tank behavior as expected...")
     
     
 class BeanSupplyTest(unittest.TestCase):
